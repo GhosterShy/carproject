6623e827fb24339bfb7ca4fbb11d9d39a77d42c0
Shyngys Erzhurek <152967828+GhosterShy@users.noreply.github.com>
2025-12-08 20:09:03 +0500

    last_chnges

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,5 +1,6 @@
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:8080";
+const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
+console.log(API_BASE);
 export const api = (endpoint, options = {}) => {
   return fetch(`${API_BASE}/${endpoint}`, {
     credentials: 'include',
